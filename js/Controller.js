756ea998a82e2d9321e06b83c61b13df760e540a
Eduardo Lopez <eduardo.lopez@amaris.com>
2017-06-07 16:03:24 +0200

    dfg

--- js/Controller.js
+++ js/Controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 define([
   'underscore',
   'marionette',
@@ -14,25 +13,4 @@ define([
       }      
       
   });
-
-  
-=======
-define([
-  'underscore',
-  'marionette',
-  'backbone'
-], 
- function(_, Marionette, Backbone){
- 
-  return Marionette.Object.extend({
-      showAbout : function(data){
-      	console.log('entro desde el controller, con data ->'+ data);
-  		this.triggerMethod('pulaMorena');
-
-      }      
-      
-  });
-
-  
->>>>>>> bc2ad9a4eeb3ed8262519a8ebc4e04a5c3d5ac57
 });
